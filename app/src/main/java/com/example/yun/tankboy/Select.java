54498e2212b862ebb8bc58a28f79e9a58fc67203
Heain <itlockit@gmail.com>
2018-08-24 16:32:54 +0900

    select activity 진행중

--- app/src/main/java/com/example/yun/tankboy/Select.java
+++ app/src/main/java/com/example/yun/tankboy/Select.java
@@ -2,6 +2,7 @@ package com.example.yun.tankboy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Select extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class Select extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
+
+        // 가전 제품 선택 설명
+        TextView selectExplainTextView = (TextView) findViewById(R.id.SelectExplainTextView);
+        selectExplainTextView.setTextSize(Intro.width_pixel / 60);
     }
 }
